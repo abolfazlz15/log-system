18e84043ea73d88cc5a653731b1f58c022f5a71d
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-12-13 09:22:04 +0330

    feat: add send error email task

---
+++ monitoring/tasks.py
@@ -0,0 +1,12 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from core.settings import EMAIL_HOST_USER, EMAIL_SUPPORT
+
+@shared_task
+def send_error_email(log_data):
+    subject = 'Error Notification'
+    message = f'An error occurred:\n\n{log_data}'
+    from_email = EMAIL_HOST_USER
+    to_email = EMAIL_SUPPORT 
+
+    send_mail(subject, message, from_email, [to_email])
