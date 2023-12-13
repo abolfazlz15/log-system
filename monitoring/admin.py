d75d8562f886edfc8e5ca2d5acf4c0e48514cf95
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-12-13 11:47:48 +0330

    feat: add filter for logs in django admin

--- monitoring/admin.py
+++ monitoring/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import LogEntry, StatusCodeList, LogController
 
-admin.site.register(LogEntry)
-admin.site.register(LogController)
+from django.contrib import admin
+from .models import LogEntry
+
+
+
+
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ['user', 'method', 'status', 'date_time']
+    list_filter = ['method', 'status', 'date_time']
+
+class LogControllerAdmin(admin.ModelAdmin):
+    list_display = ['is_active']
+
+
+admin.site.register(LogEntry, LogEntryAdmin)
+admin.site.register(LogController, LogControllerAdmin)
 admin.site.register(StatusCodeList)
